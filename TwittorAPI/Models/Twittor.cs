1df58e719548cfb9a7447e0f13fd68d17b4d3dac
arizalpratama <arizalpratama01@gmail.com>
2022-01-13 07:15:32 +0700

    add models using scaffold db context

---
+++ TwittorAPI/Models/Twittor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace TwittorAPI.Models
+{
+    public partial class Twittor
+    {
+        public Twittor()
+        {
+            Comments = new HashSet<Comment>();
+        }
+
+        public int Id { get; set; }
+        public string Twit { get; set; }
+        public DateTime Created { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
---
+++ TwittorAPI/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace TwittorAPI.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Profiles = new HashSet<Profile>();
+            UserRoles = new HashSet<UserRole>();
+        }
+
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public virtual ICollection<Profile> Profiles { get; set; }
+        public virtual ICollection<UserRole> UserRoles { get; set; }
+    }
+}
