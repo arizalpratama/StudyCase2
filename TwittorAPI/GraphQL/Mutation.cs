f2b939eb34be3effad68b8d6c7a58d8503a9e9bb
arizalpratama <arizalpratama01@gmail.com>
2022-01-13 09:03:05 +0700

    add mutation

--- TwittorAPI/GraphQL/Mutation.cs
+++ TwittorAPI/GraphQL/Mutation.cs
@@ -1,9 +1,14 @@
 ﻿using HotChocolate;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using TwittorAPI.Kafka;
 using TwittorAPI.Models;
@@ -134,6 +139,136 @@ namespace TwittorAPI.GraphQL
 
 
             return await Task.FromResult(ret);
+        }
+
+
+        //Add User Role
+        public async Task<TransactionStatus> AddUserRoleAsync(
+            UserRoleInput input,
+            [Service] TwittorDbContext context,
+            [Service] IOptions<KafkaSettings> kafkaSettings)
+        {
+            var userrole = new UserRole
+            {
+                UserId = input.UserId,
+                RoleId = input.UserId
+            };
+            var key = "userrole-add-" + DateTime.Now.ToString();
+            var val = JObject.FromObject(userrole).ToString(Formatting.None);
+            var result = await KafkaHelper.SendMessage(kafkaSettings.Value, "userrole-add", key, val);
+            await KafkaHelper.SendMessage(kafkaSettings.Value, "logging", key, val);
+
+            var ret = new TransactionStatus(result, "");
+            if (!result)
+                ret = new TransactionStatus(result, "Failed to submit data");
+
+
+            return await Task.FromResult(ret);
+        }
+
+        //Update User Role
+        public async Task<TransactionStatus> UpdateUserRoleAsync(
+            UserRoleInput input,
+            [Service] TwittorDbContext context,
+            [Service] IOptions<KafkaSettings> kafkaSettings)
+        {
+            var userrole = context.UserRoles.Where(o => o.Id == input.Id).FirstOrDefault();
+            if (userrole != null)
+            {
+                userrole.UserId = input.UserId;
+                userrole.RoleId = input.RoleId;
+
+                context.UserRoles.Update(userrole);
+                await context.SaveChangesAsync();
+            }
+            var key = "userrole-update-" + DateTime.Now.ToString();
+            var val = JObject.FromObject(userrole).ToString(Formatting.None);
+            var result = await KafkaHelper.SendMessage(kafkaSettings.Value, "userrole-update", key, val);
+            await KafkaHelper.SendMessage(kafkaSettings.Value, "logging", key, val);
+
+            var ret = new TransactionStatus(result, "");
+            if (!result)
+                ret = new TransactionStatus(result, "Failed to update data");
+
+
+            return await Task.FromResult(ret);
+        }
+
+        //Register
+        public async Task<UserData> RegisterUserAsync(
+            RegisterUser input,
+            [Service] TwittorDbContext context)
+        {
+            var user = context.Users.Where(o => o.Username == input.UserName).FirstOrDefault();
+            if (user != null)
+            {
+                return await Task.FromResult(new UserData());
+            }
+            var newUser = new User
+            {
+                FullName = input.FullName,
+                Email = input.Email,
+                Username = input.UserName,
+                Password = BCrypt.Net.BCrypt.HashPassword(input.Password)
+            };
+
+            var ret = context.Users.Add(newUser);
+            await context.SaveChangesAsync();
+
+            return await Task.FromResult(new UserData
+            {
+                Id = newUser.Id,
+                Username = newUser.Username,
+                Email = newUser.Email,
+                FullName = newUser.FullName
+            });
+        }
+
+        //Login
+        public async Task<UserToken> LoginAsync(
+           LoginUser input,
+           [Service] IOptions<TokenSettings> tokenSettings,
+           [Service] TwittorDbContext context)
+        {
+            var user = context.Users.Where(o => o.Username == input.Username).FirstOrDefault();
+            if (user == null)
+            {
+                return await Task.FromResult(new UserToken(null, null, "Username or password was invalid"));
+            }
+            bool valid = BCrypt.Net.BCrypt.Verify(input.Password, user.Password);
+            if (valid)
+            {
+                var securitykey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenSettings.Value.Key));
+                var credentials = new SigningCredentials(securitykey, SecurityAlgorithms.HmacSha256);
+
+                var claims = new List<Claim>();
+                claims.Add(new Claim(ClaimTypes.Name, user.Username));
+                var userRoles = context.UserRoles.Where(o => o.UserId == user.Id).ToList();
+
+                foreach (var userRole in user.UserRoles)
+                {
+                    var role = context.Roles.Where(o => o.Id == userRole.RoleId).FirstOrDefault();
+                    if (role != null)
+                    {
+                        claims.Add(new Claim(ClaimTypes.Role, role.Name));
+                    }
+                }
+
+                var expired = DateTime.Now.AddHours(3);
+                var jwtToken = new JwtSecurityToken(
+                    issuer: tokenSettings.Value.Issuer,
+                    audience: tokenSettings.Value.Audience,
+                    expires: expired,
+                    claims: claims,
+                    signingCredentials: credentials
+                );
+
+                return await Task.FromResult(
+                    new UserToken(new JwtSecurityTokenHandler().WriteToken(jwtToken),
+                    expired.ToString(), null));
+            }
+
+            return await Task.FromResult(new UserToken(null, null, Message: "Username or password was invalid"));
         }
     }
 }
